2331d5a9dd31160e9cb532758bf3f2793bbb499b
alejandro <alejandro.delasencanales2@gmail.com>
2019-10-18 19:38:44 +0200

    else_a.php

---
+++ else_a.php
@@ -0,0 +1,19 @@
+<form>
+	<input name="note"/>
+	</br>
+	<button>Comprobar</button>
+	</br>
+</form>
+
+<?php
+if (isset($_GET['nota'])){
+	if ($_GET['nota']>=0 && $_GET['nota']<=10){
+		if ($_GET['nota']>=5){
+			echo 'Aprobado';
+		}else
+			echo 'Suspenso';
+	}else
+		echo 'Mete un número del 0 al 10 o no funciono.';
+}else
+	echo 'Mete una nota arriba del 0 al 10.';
+?>
