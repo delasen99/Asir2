2bc2b921f18a0608150d9323b6fcf68042bc10c1
unknown <alejandro.delasencanales2@gmail.com>
2019-12-02 15:40:15 +0100

    html.php

---
+++ html.php
@@ -0,0 +1,67 @@
+<html>
+
+<head>
+<meta charset="UTF-8"/>
+<title>PAGINA HTML</title>
+</head>
+
+<body bgcolor="CFD2CF" marginwidth="25">
+
+<center>
+
+<h1>PROVINCIAS BAÑADAS POR EL CANTABRICO</h1>
+
+<img src="ibiza2020.jpg" alt="Coche más vendido del año" width="400" height="300"/>
+</table>
+</center>
+
+<form>
+	<input/>
+	<input type="submit"/>
+</form>
+
+<h2><font size="6", color="brown"><b><u>
+Coches preferidos de la gente joven:
+</font>
+</b></u></h2>
+
+<table border="1" width="250" align="center">
+
+<tr align="center">
+<th><font size="4"> Año </font></th>
+<th><font size="4"> Coche </font></th>
+</tr>
+
+<tr align="center">
+<td><font color="blue", size="4"> 2016 </font></td>
+<td><font color="blue", size="4"> Corsa </font></td>
+</tr>
+
+<tr align="center">
+<td><font color="blue", size="4"> 2017 </font></td>
+<td><font color="blue", size="4"> Golf </font></td>
+</tr>
+
+<tr align="center">
+<td><font color="F4BD02", size="4"> 2018 </font></td>
+<td><font color="F4BD02", size="4"> Audi rs3 </font></td>
+</tr>
+
+<tr align="center">
+<td><font color="red", size="4"> 2019 </font></td>
+<td><font color="red", size="4"> Ibiza </font></td>
+</tr>
+
+</table>
+
+<p><b><font color="E502F4", size="5">
+Como podemos ver en la tabla, los resultados son muy variopintos, siendo el golf el coche con más ventas de todos los de la tabla.
+</font>
+</b></p>
+
+<h1>Golf 2.0</h1>
+<h2>Golg GTI</h2>
+<h3>Golf R </h3>
+
+</body>
+</html>
